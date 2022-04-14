be65187c116d085740978758415bcec85dae17b2
Lash Blast <evelyn.aguirre.9895@gmail.com>
2022-04-14 18:01:37 -0500

    set up server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+// const PORT = process.env.PORT || 3001;
+const app = express();
+// const apiRoutes = require('./Routes/apiRoutes');
+// const htmlRoutes = require('./Routes/htmlRoutes');
+
+
+
+
+
+app.listen(3001, () => {
+  console.log(`API server now on port 3001!`);
+});
