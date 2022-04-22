ef96062a3cba2958356b69bb5f8beca72eb3cdb7
Lash Blast <evelyn.aguirre.9895@gmail.com>
2022-04-22 16:21:08 -0500

    save current code

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,26 +1,90 @@
 const router = require('express').Router();
 // const { filterByQuery, findById,} = require('../../lib/notes');
-const { notes } = require('../../db/db');
+const { createNewNote } = require('../../lib/notes.js');
+const { notes } = require('../../db/db.json');
+const uuid = require('../../lib/uuid.js');
+
 const fs = require("fs");
+const path = require('path');
 
 
-router.get('/notes', (req, res) => {
-  let results = notes;
-  // if (req.query) {
-  //   results = filterByQuery(req.query, results);
-  // } 
-  res.json(results);
+// request for notes
+router.get('/api/notes', (req, res) => {
+  res.json(notes);
 });
 
-// router.post('/api/notes', (req, res) => {
-//   // req.body is where our incoming content will be
-//   console.log(req.body);
-//   res.json(req.body);
-// });
+// GET request for a single note
+router.get('/api/notes/:note_id', (req, res) => {
+  if (req.body && req.params.note_id) {
+    const noteId = req.params.note_id;
+    for (let i = 0; i < notes.length; i++) {
+      const currentNote = notes[i];
+      if (currentNote.note_id === noteId) {
+        res.json(currentNote);
+        return;
+      }
+    }
+    res.json('Note ID not found');
+  }
+});
+
+
+
+// POST request to add a note
+router.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a note`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    // Obtain existing notes
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
+
+        // Add a new note
+        parsedNotes.push(newNote);
+
+        // Write updated notes back to the file
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated notes!')
+        );
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.json(response);
+  } else {
+    res.json('Error in posting note');
+  }
+});
 
 
 // length undefined TypeError
-router.post('/notes', (req, res) => {
+router.post('/api/notes', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = notes.length.toString();
 
